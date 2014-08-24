0f4aafa5d63a8283c3f0ded68eed41c972fcc8a0
Fatty Arbuckle <fatty.arbuckle.87@gmail.com>
2014-08-24 18:21:45 -0400

    Moved functions out of code and into util class Finished challenge 6

--- set_1/challenge_6.py
+++ set_1/challenge_6.py
@@ -1,43 +1,79 @@
 #! /usr/bin/python
-import string
 import sys
-import base64
+import util
 
-def b64ToByteString(b64):
-	return base64.b64decode(b64)
-
-def hexStringToByteArray(hexString):
-	return stringToByteArray(hexString, 2, lambda x: int(x, 16))
-
-def stringToByteArray(s, byteSize=1, convert=lambda x: ord(x)):
-	for i in xrange(0, len(s), byteSize):
-		yield convert(s[i:i+byteSize])
-
-def calcHamming(a, b):
-	assert len(a) == len(b)
-	ham = 0
-	for i in xrange(0, len(a)):
-		v = a[i] ^ b[i]
-		while v > 0:
-			ham += 1
-			v &= v - 1
-	return ham
+### ======================================
+###
+### load the input data
 
 input_bytes = bytearray()
 
 with open(sys.argv[1], 'r') as fin:
 	for input in fin:
-		input_bytes += b64ToByteString(input.rstrip())
+		input_bytes += util.b64ToByteString(input.rstrip())
+
+### ======================================
+###
+### Find the likely key sizes
 
 hams = []
 for key_size in range(2, 40):
-	block_a = input_bytes[0:key_size]
-	block_b = input_bytes[key_size:2 * key_size]
-	nHam = calcHamming(block_a, block_b) / key_size
-	hams.append({'key_size': key_size, 'ham': nHam})
+	h = util.ham_n_blocks(input_bytes, 4, key_size)
+	hams.append({'key_size': key_size, 'ham': h})
 
-s = sorted(hams, key=lambda x: x['ham'])
-best_key_size = s[0]['key_size']
+sorted_keys = sorted(hams, key=lambda x: x['ham'])
 
-print best_key_size
+### ======================================
+###
+### Try the likely key sizes
+
+best_result = { 'chi': 1000000.0, 'plaintext': "error", 'key': "error" }
+limiter = 0
+for key in sorted_keys:
+	limiter += 1
+	if limiter > 10:
+		break
+		
+	key_size = key['key_size']
+
+	## Create blocks of key_size stripes
+
+	block = []
+	for i in range(0, key_size):
+		block.append([])
+
+	b = 0
+	while b < len(input_bytes):
+		for i in range(0, key_size):
+			if b < len(input_bytes):
+				block[i].append(input_bytes[b])
+				b += 1
+
+	## Find the best single byte key for each block
+
+	final_key = []
+	for i in range(0, key_size):
+		k = util.bestSingleXorByte(block[i])
+		final_key.append(k)
+
+	## construct a long key to xor against
+
+	xor_bytes = bytearray()
+	while len(xor_bytes) < len(input_bytes):
+		for l in final_key:
+			if len(xor_bytes) < len(input_bytes):
+				xor_bytes.append(l)
+
+	## calculate the xor and test the plaintext
+	
+	tmp = bytearray(util.xorByteArrays(xor_bytes, input_bytes))
+	freq = util.letterFrequency(tmp)
+	chi = util.chiSquare(freq)
+	if (chi < best_result['chi']):
+		best_result['chi'] = chi
+		best_result['plaintext'] = tmp
+		best_result['key'] = final_key
+
+print "Best key: " + str(bytearray(best_result['key'])) + "; plaintext: "
+print best_result['plaintext']
 
---
+++ set_1/util.py
@@ -0,0 +1,91 @@
+import string
+import base64
+
+
+def b64ToByteString(b64):
+	return base64.b64decode(b64)
+
+def hexStringToByteArray(hexString):
+	return stringToByteArray(hexString, 2, lambda x: int(x, 16))
+
+def stringToByteArray(s, byteSize=1, convert=lambda x: ord(x)):
+	for i in xrange(0, len(s), byteSize):
+		yield convert(s[i:i+byteSize])
+
+def calcHamming(a, b):
+	assert len(a) == len(b)
+	ham = 0
+	for i in xrange(0, len(a)):
+		v = a[i] ^ b[i]
+		while v > 0:
+			ham += 1
+			v &= v - 1
+	return ham
+	
+def ham_n_blocks(input_bytes, n, key_size):
+	assert n>1
+	assert (n+1)*key_size < len(input_bytes)
+	block = []
+	for i in range(0, n):
+		block.append([])
+		block[i] = input_bytes[i * key_size:(i+1) * key_size]
+	h = 0
+	comparisons = 0
+	for i in range(0, n):
+		for j in range(i+1, n):
+			h += calcHamming(block[i], block[j]) / key_size
+			comparisons += 1
+	return h/n
+
+# xor generator
+def xorByteArrays(first, second):
+	assert len(first) == len(second)
+	for i in range(0, len(first)):
+		yield first[i] ^ second[i]
+
+# from http://en.wikipedia.org/wiki/Letter_frequency
+ENGLISH_LETTER_FREQUENCY = [ 
+	 .8167,  .1492,  .2782,  .4253, 1.2702,
+	 .2228,  .2015,  .6094,  .6966,  .0153,
+	 .0772,  .4025,  .2406,  .6749,  .7507,
+	 .1929,  .0095,  .5987,  .6327,  .9056,
+	 .2758,  .0978,  .2360,  .0150,  .1974,
+	 .0074,  .0001 ]
+	 
+NON_PRINTABLE_INDEX=26
+
+# get letter frequency (plus non-printables and average work length
+def letterFrequency(input):
+	count = len(input)
+	freq = [ 0.0 ] * len(ENGLISH_LETTER_FREQUENCY)
+	for i in range(0, len(input)):
+		if chr(input[i]).isalpha():
+			freq[ord(chr(input[i]).lower()) - ord('a')] += 1.0
+		if chr(input[i]) not in string.printable:
+			freq[NON_PRINTABLE_INDEX] += count*10000
+	for i in range(0, len(freq)):
+		freq[i] /= count
+	return freq
+
+# chiSquare compare
+def chiSquare(observed, expected=ENGLISH_LETTER_FREQUENCY):
+	assert len(observed) == len(expected)
+	sum = 0.0
+	for i in range(0, len(observed)):
+		sum += ( (observed[i] - expected[i])**2 ) / expected[i]
+	return sum
+
+def bestSingleXorByte(s):
+	chiMap = {}
+	for xorByte in range(0, 256):
+		key = bytearray()
+		while (len(key) < len(s)):
+			key.append(xorByte)
+		tmp = bytearray(xorByteArrays(s, key))
+		freq = letterFrequency(tmp)
+		chi = chiSquare(freq, ENGLISH_LETTER_FREQUENCY)
+		chiMap[xorByte] = {'input': s, 'key': xorByte, 'text': tmp, 'freq': freq, 'chi': chi }
+
+	results = sorted(chiMap.iteritems(), key=lambda kvt: kvt[1]['chi'])
+	return results[0][1]['key']
+
